b0d6da1289c4d87e7da00709585e9fe3d99a552d
Zoltán Csorba <zcsorba@mobacar.com>
2018-12-07 10:58:20 +0000

    sample project for checking Infinitest working with STS 3.9.6

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+	
+	@Value("${spring.data.mongodb.host}")
+	private String valueProp;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	public String getValueProp() {
+		return valueProp;
+	}
+}
